eb683ab2a097faae5b7e9dfee51a907c0f1d5e65
Andrey Zimovnov <zimovnov@gmail.com>
2017-09-27 22:59:32 +0300

    users

--- generate_users.sh
+++ generate_users.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo "" > passwords
+pass=`openssl rand -hex 6` # generate random password
 for i in `seq 1 32`; do
     user="user$i"
     useradd -m $user # make the user
-    pass=`openssl rand -hex 8` # generate random password
     echo "$user:$pass" | chpasswd # set the password
     echo "$user $pass" >> passwords
 done
